953b0ad6ab4a43bcbf02864dfac54f838eeb1904
Thimot VEYRE <thimot.veyre@epita.fr>
2020-01-29 11:02:32 +0100

    ajout d'une tuile

--- Assets/Scenes/Liquid/Liquid_2.cs
+++ Assets/Scenes/Liquid/Liquid_2.cs
@@ -11,7 +11,14 @@ public class Liquid_2 : TileMap
     
     public override void _Ready()
     {
+        ground = this;
+        Array test = ground.TileSet.GetTilesIds();
+        foreach (var test2 in test)
+        {
+            GD.Print(test2);
+        }
         
+        ground.SetCell(1,2,0);
     }
 
     public void Liquid_3(Type type)
@@ -26,9 +33,7 @@ public class Liquid_2 : TileMap
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
      public override void _Process(float delta)
-     { 
-         TileSet water = (TileSet) GetTree().GetRoot().GetNode("Liquid_2").GetNode("Liquid");
-        Array test = water.GetTilesIds();
-        ground.SetCell(1,2, (int) test[0]);
-    }
+     {
+         //
+     }
 }
