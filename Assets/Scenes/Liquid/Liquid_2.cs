9fb7a86549e000ed68d211a25269272506ba257d
Thimot VEYRE <thimot.veyre@epita.fr>
2020-01-30 16:51:14 +0100

    modification aleatoire des tuiles

--- Assets/Scenes/Liquid/Liquid_2.cs
+++ Assets/Scenes/Liquid/Liquid_2.cs
@@ -4,6 +4,11 @@ using Array = Godot.Collections.Array;
 
 public class Liquid_2 : TileMap
 {
+    //private int i = 70;
+    //private int j = 0;
+    private TileMap ground2;
+    private float Sdelta = 0;
+    Random rand = new Random();
     public enum Type 
     { WATER, OIL }
 
@@ -11,19 +16,14 @@ public class Liquid_2 : TileMap
     
     public override void _Ready()
     {
-        ground = this;
-        Array test = ground.TileSet.GetTilesIds();
-        foreach (var test2 in test)
-        {
-            GD.Print(test2);
-        }
-        
-        ground.SetCell(1,2,0);
+       ground = (TileMap) this.GetParent();
+       ground2 = this;
+       //ground = (TileMap) GetTree().GetRoot().GetNode("res://Assests/Scenes/Liquid/Liquid_2");
     }
 
     public void Liquid_3(Type type)
     {
-        ground = (TileMap) GetTree().GetRoot().GetNode("Liquid_2").GetNode("Ground");
+        
     }
     
     private void Neighbour()
@@ -31,9 +31,43 @@ public class Liquid_2 : TileMap
         
     }
 
+    private void Map(TileMap map)
+    {
+    }
+
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
      public override void _Process(float delta)
      {
-         //
+         Sdelta += delta;
+         if (Sdelta >= 1)
+         {
+             Dessin();
+             Sdelta = 0;
+         }
+             
+          ground2.SetCell(60,0,1,false);
+     }
+
+     private void Dessin()
+     {
+       int[,] map = new int[50,90];
+         for(int i = 0; i < 50; i++)
+         {
+             for (int j = 0; j < 90; j++)
+             {
+                 map[i, j] = ground.GetCell(i, j);
+             }
+         }
+       
+         for(int i = 0; i < map.GetUpperBound(0);i++)
+         {
+             for(int j = 0; j < map.GetUpperBound(1); j++)
+             {
+                 if (map[i,j] == 0 || map[i,j] == 1 || map[i,j] == 2)
+                 map[i, j] = rand.Next(0, 2);
+                 ground.SetCell(i,j,map[i,j]);
+                 
+             }
+         }  
      }
 }
