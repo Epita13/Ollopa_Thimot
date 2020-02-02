167459742015a1c8019b5d954ab441bab3403904
Thimot VEYRE <thimot.veyre@epita.fr>
2020-02-02 21:04:15 +0100

    Intégration eau v1
    Reste quelques bug et choses à faire

--- Assets/Scenes/Liquid/Liquid_2.cs
+++ Assets/Scenes/Liquid/Liquid_2.cs
@@ -4,189 +4,230 @@ using Array = Godot.Collections.Array;
 
 public class Liquid_2 : TileMap
 {
-    private float Sdelta = 0;
-    private TileMap ground;
-    private TileMap waterMap;
-    private int capacity = 8;        //nombre d'etages de l'eau, 0 = pas d'eau, 8 = cube plein;
-    public enum Type 
-    { WATER, OIL }
+	/*Il reste un petit soucis, c'est que de l'eau se teleporte plus loin lorsque on ne la limite plus a une cuvette*/
+	
+	
+	/*Pour utiliser l'eau, il suffit d'appeler la fonction DrawWaterLevel(), pour les niveaux, le niveau max est
+	 défini par capacity. Pour fonctionner correctement le TileSet associé doit contenir au minimum un sprite pour chaque
+	 niveau. Numeroté de 1 a capacity. le sprite 0 doit OBLIGATOIREMENT etre un sprite transparent*/
+	
+	private float Sdelta = 0;
+	private TileMap ground;
+	private TileMap waterMap;
+	private int capacity = 8;
+	public enum Type 
+	{ WATER, OIL }
 
-    
-    
-    public override void _Ready()
-    {
-       ground = (TileMap) this.GetParent();
-       waterMap = this;
-    }
+	
+	
+	public override void _Ready() /*doit appeller chaque tilemap presente sur le jeux, pas encore fait, surement mettre une liste*/
+	{
+	   ground = (TileMap) this.GetParent();
+	   waterMap = this;
+	}
+	
+	public override void _Process(float delta)
+	 {
+		 Sdelta += delta;
+		 DrawWaterLevel();
+		 if (Sdelta >= 1)
+		 {
+			 
+			 Sdelta = 0;
+		 }
+	 }
+	 
+	 public void DrawWaterLevel()
+	 {
+		 /*Récupere les niveaux et emplacement d'eau puis calcule les nouveux niveau verticaux puis horizontaux*/
+		 
+		 int[,] map = GetWaterLevel();
+		 map = VerticalWater(map);
+		 DrawWater(map);
+		 DrawWater(HorizontalWater(map));
+	 }
+	 
+	 private int[,] GetWaterLevel()			/*ajouter le prise en compte de toute les TileMap*/
+	 {
+		 /*Recuperation des emplacements de blocks et d'eau sur toutes les TileMap*/
+		 
+		 int[,] map = new int[50,50]; 		//a modifier pour prendre la taille window*/
+		 for(int x = 0; x <= map.GetUpperBound(0); x++)
+		 {
+			 for (int y = 0; y <= map.GetUpperBound(1); y++)
+			 {
+				 if (ground.GetCell(x, y) != -1)
+					 map[x, y] = 0;
+				 else
+					 map[x, y] = -1;
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-     public override void _Process(float delta)
-     {
-         Sdelta += delta;
-         
-         if (Sdelta >= 1)
-         {
-             DrawWaterLevel();
-             Sdelta = 0;
-         }
-     }
-     
-     private void DrawWaterLevel()
-     {
-         int[,] map = GetWaterLevel();
-         map = VerticalWater(map);
-         DrawWater(map);
-         DrawWater(HorizontalWater(map));
-     }
-     
-     private int[,] GetWaterLevel()
-     {
-         /*Recuperation des valeurs*/
-         int[,] map = new int[50,50];
-         for(int x = 0; x <= map.GetUpperBound(0); x++)
-         {
-             for (int y = 0; y <= map.GetUpperBound(1); y++)
-             {
-                 if (ground.GetCell(x, y) != -1)
-                     map[x, y] = 0;
-                 else
-                     map[x, y] = -1;
+				 if (map[x, y] == -1 && waterMap.GetCell(x, y) > 0)
+					 map[x, y] = waterMap.GetCell(x, y);
+			 }
+			 
+		 }
+		 return map;
+	 }
 
-                 if (map[x, y] == -1 && waterMap.GetCell(x, y) > 0)
-                     map[x, y] = waterMap.GetCell(x, y);
-             }
-             
-         }
-         return map;
-     }
+	 private int[,] HorizontalWater(int[,] map)
+	 {
+		 /*Calcule la difference d'eau avec les tuiles voisines d'une tuiles contenant de l'eau.
+		  Redefinit ensuite le nouveau niveau en fonction des blocks deja present*/
+		 
+		 /*Peut surement être simplifier*/
+		 for (int x = 0; x < map.GetUpperBound(0); x++)
+		 {
+			 for (int y = 0; y < map.GetUpperBound(1); y++)
+			 {
+				 if (map[x, y] > 0)
+				 {
+					 int differenceLeft = 0;
+					 int differenceRight = 0;
+					 
+					 if (x > 0)
+					 {
+						 if (map[x + 1, y] == -1)
+							 differenceRight = capacity;
+						 else if (map[x + 1, y] != 0 && map[x + 1,y] < map[x,y])
+							 differenceRight = capacity - map[x + 1, y];
+					 }
+					 
+					 if(x < map.GetUpperBound(0))
+					 {
+						 if (map[x - 1, y] == -1)
+							 differenceLeft = capacity;
+						 else if (map[x - 1, y] != 0 && map[x - 1,y] < map[x,y])
+							 differenceLeft = capacity - map[x - 1, y];
+					 }
+					 
 
-     private int[,] HorizontalWater(int[,] map)
-     {
-         for (int x = 0; x <= map.GetUpperBound(0); x++)
-         {
-             for (int y = 0; y <= map.GetUpperBound(1); y++)
-             {
-                 int difference_left = 0;
-                 int difference_right = 0;
-                 
-                 if (map[x, y] != 0)
-                 {
-                     if (x > 0 && x < map.GetUpperBound(0))
-                     {
-                         if (map[x - 1, y] == -1)
-                             difference_left = capacity;
-                         else if (map[x - 1, y] != 0)
-                             difference_left = capacity - map[x - 1, y];
-                         
-                         if (map[x + 1, y] == -1)
-                             difference_right = capacity;
-                         else if (map[x + 1, y] != 0)
-                             difference_right = capacity - map[x + 1, y];
-                     } 
-                     else if (x == 0)
-                     {
-                         if (map[x + 1, y] == -1)
-                             difference_right = capacity;
-                         else if (map[x + 1, y] != 0)
-                             difference_right = capacity - map[x + 1, y];              
-                     }
-                     else
-                     {
-                         if (map[x - 1, y] == -1)
-                             difference_left = capacity;
-                         else if (map[x - 1, y] != 0)
-                             difference_left = capacity - map[x - 1, y];               
-                     }  
-                 }
+					 if (x > 0 && x < map.GetUpperBound(0) && map[x - 1, y] != 0 && map[x + 1, y] != 0)
+					 {
+						 if (differenceLeft > differenceRight)
+						 {
+							 if (map[x - 1, y] > 0 && map[x - 1, y] < 8)
+							 {
+								 map[x - 1, y]++;
+								 map[x, y]--;
+							 }
+							 else if (map[x - 1, y] == -1)
+							 {
+								 map[x - 1, y] = 1;
+								 map[x, y]--;
+							 }
+						 }
+						 else if (differenceLeft <= differenceRight)
+						 {
+							 if (map[x + 1, y] > 0 && map[x + 1, y] < 8)
+							 {
+								 map[x + 1, y]++;
+								 map[x, y]--;
+							 }
+							 else if (map[x + 1, y] == -1)
+							 {
+								 map[x + 1, y] = 1;
+								 map[x, y]--;
+							 }
+						 }
+						 else if (differenceLeft == 1 && differenceRight == 1)
+						 {
+							 if (map[x + 1, y] > 0 && map[x + 1, y] < 8)
+							 {
+								 map[x + 1, y]++;
+								 map[x, y]--;
+							 }
+							 else if (map[x + 1, y] == -1)
+							 {
+								 map[x + 1, y] = 1;
+								 map[x, y]--;
+							 }
+						 }
+					 }
+					 else if ((x == 0 || map[x - 1, y] == 0) && map[x + 1, y] != 0 && differenceRight != 0)
+					 {
+						 if (map[x + 1, y] > 0 && map[x + 1, y] < 8)
+						 {
+							 map[x + 1, y]++;
+							 map[x, y]--;
+						 }
+						 else if (map[x + 1, y] == -1)
+						 {
+							 map[x + 1, y] = 1;
+							 map[x, y]--;
+						 }
+					 }
+					 else if ((map[x + 1, y] == 0 || x == map.GetUpperBound(0)) && map[x - 1, y] != 0 && differenceLeft != 0)
+					 {
+						 if (map[x - 1, y] > 0 && map[x - 1, y] < 8)
+						 {
+							 map[x - 1, y]++;
+							 map[x, y]--;
+						 }
+						 else if (map[x - 1, y] == -1)
+						 {
+							 map[x - 1, y] = 1;
+							 map[x, y]--;
+						 }
+					 }
+				 }
+			 }
+		 }
+		 return map;
+	 }
+	 
+	 private int[,] VerticalWater(int[,] map)
+	 {
+		 /*Calcule pour toutes les tuiles vide la difference d'eau avec la tuile au dessus.
+		  Transfert l'eau si possible*/
+		 
+		 for (int x = 0; x <= map.GetUpperBound(0); x++)
+		 {
+			 for (int y = map.GetUpperBound(1); y >= 0; y--)
+			 {
+				 if (y > 0 && map[x, y - 1] > 0 && map[x,y] != 0)
+				 {
+					 if (map[x, y] <= -1)
+					 {
+						 map[x, y] += map[x, y - 1] + 1;
+						 map[x, y - 1] = -1; 
+					 }
+					 else
+					 {
+						 int difference = capacity - map[x, y];
+						 if(map[x, y - 1] < difference)
+						 {
+							 map[x, y] += map[x, y - 1];
+							 map[x, y - 1] = -1;                                                      
+						 }
+						 else
+						 {
+							 map[x, y] = capacity;
+							 map[x, y - 1] -= difference;
+						 }
+					 }
 
-                 if (difference_left != 0 && difference_right != 0)
-                 {
+					 if (map[x, y] == 0)
+						 map[x, y] = -1;
+					 if (map[x, y - 1] == 0)
+						 map[x, y - 1] = -1;
+				 }
+			 }
+		 }
 
-                     if (difference_left > difference_right && map[x, y] > 1)
-                     {
+		 return map;
+	 }
 
-                         if (map[x - 1, y] > 0 && map[x - 1, y] < 8)
-                         {
-                             map[x - 1, y]++;
-                             map[x, y]--;
-                         }
-                         else if (map[x - 1, y] == -1)
-                         {
-                             map[x - 1, y] = 1;
-                             map[x, y]--;
-                         }
-
-                     }
-                     else if (map[x, y] > 1)
-                     {
-                         if (map[x + 1, y] > 0 && map[x + 1, y] < 8)
-                         {
-                             map[x + 1, y]++;
-                             map[x, y]--;
-                         }
-                         else if (map[x - 1, y] == -1)
-                         {
-                             map[x + 1, y] = 1;
-                             map[x, y]--;
-                         }
-                     }
-                 }
-
-             }
-         }
-         return map;
-     }
-     
-     private int[,] VerticalWater(int[,] map)
-     {
-         for (int x = 0; x <= map.GetUpperBound(0); x++)
-         {
-             for (int y = map.GetUpperBound(1); y >= 0; y--)
-             {
-                 if (y > 0 && map[x, y - 1] > 0 && map[x,y] != 0)
-                 {
-                     if (map[x, y] <= -1)
-                     {
-                         map[x, y] += map[x, y - 1] + 1;
-                         map[x, y - 1] = -1; 
-                     }
-                     else
-                     {
-                         int difference = capacity - map[x, y];
-                         if(map[x, y - 1] < difference)
-                         {
-                             map[x, y] += map[x, y - 1];
-                             map[x, y - 1] = -1;                                                      
-                         }
-                         else
-                         {
-                             map[x, y] = capacity;
-                             map[x, y - 1] -= difference;
-                         }
-                     }
-
-                     if (map[x, y] == 0)
-                         map[x, y] = -1;
-                     if (map[x, y - 1] == 0)
-                         map[x, y - 1] = -1;
-                 }
-             }
-         }
-
-         return map;
-     }
-
-     
-     private void DrawWater(int [,] waterLevel)
-     {
-         /*Dessine sur la Tilemap les niveaux d'eau correspondant à la matrice*/
-         
-         for (int x = 0; x <= waterLevel.GetUpperBound(0); x++)
-         {
-             for (int y = 0; y <= waterLevel.GetUpperBound(1); y++)
-             {
-                 waterMap.SetCell(x,y, waterLevel[x,y]);
-             }
-         }
-     }
+	 
+	 private void DrawWater(int [,] waterLevel)
+	 {
+		 /*Dessine sur la Tilemap les niveaux d'eau correspondant à la matrice*/
+		 
+		 for (int x = 0; x <= waterLevel.GetUpperBound(0); x++)
+		 {
+			 for (int y = 0; y <= waterLevel.GetUpperBound(1); y++)
+			 {
+				 waterMap.SetCell(x,y, waterLevel[x,y]);
+			 }
+		 }
+	 }
 }
